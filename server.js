fe7e1df1d63c193e5c06bc122bec23b3e2bf3e82
piyush singh <piyushsingh82@gmail.com>
2021-07-01 19:28:13 +0530

    updated server port variable

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("./connection");
 const Userdata = require("./userschema");
 const userData = require("./userschema");
 const app = express();
-const port = process.evn.PORT || 3001;
+
 
 app.use(express.json());
 const jwtkey = "samplejsonwebtoken";
@@ -138,6 +138,6 @@ function verifytoken(req, res, next) {
   }
 }
 
-app.listen(port, () => {
-  console.log("listening on port 3001...");
+app.listen((process.env.PORT ), () => {
+  console.log(`listening on port ${process.env.PORT}...`);
 });
